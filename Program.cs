3ec5371ae8cd7e08a094d44459a16f5c3c7b3c0b
marco <marco.paulo.rm@gmail.com>
2022-01-14 16:48:50 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using aula.pilha;
+using aula.fila;
+
+namespace aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /* int[] initialStack = new int[0];
+
+            PilhaRoots.Menu(initialStack); */
+
+            //Pilha.Menu(new Stack<int>());
+            Fila.Menu(new Queue<int>());
+        }
+    }
+}
